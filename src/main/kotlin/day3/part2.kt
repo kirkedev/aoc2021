0956e1489ffeec8459b84f2cbfc5106ce603ad48
Andrew Kirkegaard <andrew.kirkegaard@gmail.com>
2021-12-06 21:12:54 -0600

    Mark oxygen and scrubber rating calculation function as tail recursive

--- src/main/kotlin/day3/part2.kt
+++ src/main/kotlin/day3/part2.kt
@@ -3,22 +3,22 @@ package day3
 fun pack(bits: IntArray): UInt =
     pack(bits.map { it == 1 })
 
-fun oxygen(codes: List<IntArray>, position: Int = 0): UInt {
-    val value = countBits(codes.asSequence()).let { (count, totals) ->
+tailrec fun oxygen(codes: Iterable<IntArray>, position: Int = 0): UInt {
+    val matches = countBits(codes.asSequence()).let { (count, totals) ->
         if (totals[position] >= count / 2F) 1 else 0
+    }.let { value ->
+        codes.filter { it[position] == value }
     }
-
-    val matches = codes.filter { it[position] == value }
 
     return if (matches.size == 1) pack(matches[0]) else oxygen(matches, position + 1)
 }
 
-fun scrubber(codes: List<IntArray>, position: Int = 0): UInt {
-    val value = countBits(codes.asSequence()).let { (count, totals) ->
-        if (totals[position] < count / 2F) 1 else 0
+tailrec fun scrubber(codes: Iterable<IntArray>, position: Int = 0): UInt {
+    val matches = countBits(codes.asSequence()).let { (count, totals) ->
+        if (totals[position] >= count / 2F) 0 else 1
+    }.let { value ->
+        codes.filter { it[position] == value }
     }
-
-    val matches = codes.filter { it[position] == value }
 
     return if (matches.size == 1) pack(matches[0]) else scrubber(matches, position + 1)
 }
